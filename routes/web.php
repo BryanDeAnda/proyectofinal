d229df89a4f85f1acc3f029212b6772f55ed5339
Connie <80598372+maconniemez@users.noreply.github.com>
2023-03-31 21:57:59 -0600

    edicion routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,15 +22,3 @@ Route::get('/', function () {
 Route::resource('stocks', StockController::class);
 
 Route::resource('employees', EmployeeController::class);
-
-//Route::view('/employees', "employees")->middleware('auth')->name('employees');
-//Route::view('/employees/create', "cremp")->middleware('auth')->name('cremp');
-
-Route::view('/login', "login")->name('login');
-Route::view('/registro', "register")->name('registro');
-
-Route::post('/validar-registro',[LoginController::class, 'register'])->
-    name('validar-registro');
-Route::post('/inicia-sesion',[LoginController::class, 'login'])
-    ->name('inicia-sesion');
-Route::get('/logout',[LoginController::class,'logout'])->name('logout');
