efa14e7af92922963b9269a15f593ec8652c98d9
Connie <80598372+maconniemez@users.noreply.github.com>
2023-05-20 14:13:00 -0600

    merge coonflict comp

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\EmployeeController;
-<<<<<<< HEAD
-//use App\Http\Controllers\LoginController;
-=======
->>>>>>> connie
 
 /*
 |--------------------------------------------------------------------------
@@ -24,16 +20,6 @@ Route::get('/', function () {
 });
 
 Route::resource('employees', EmployeeController::class);
-
-//Route::view('/employees', "employees")->middleware('auth')->name('employees');
-//Route::view('/employees/create', "cremp")->middleware('auth')->name('cremp');
-
-//Route::view('/login', "login")->name('login');
-//Route::view('/registro', "register")->name('registro');
-
-//Route::post('/validar-registro',[LoginController::class, 'register'])->name('validar-registro');
-//Route::post('/inicia-sesion',[LoginController::class, 'login'])->name('inicia-sesion');
-//Route::get('/logout',[LoginController::class,'logout'])->name('logout');
 Route::resource('stocks', StockController::class);
 Route::middleware([
     'auth:sanctum',
